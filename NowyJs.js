67254785d2e2b563ec991528faaabb829e9d8633
Adam Pawlikowski <pawlikowskiadam@endor.local>
2019-10-21 11:29:16 +0200

    21.10.2019

---
+++ NowyJs.js
@@ -0,0 +1,39 @@
+var a = true;
+
+var b = 3;
+
+var c = a + b;
+
+var d = 'Ala Ma Kota';
+
+var e = c + d
+
+var zmiennaPierwsza; Camel notation 
+let ZmiennaPierwsza; Pascal notation 
+let zmienna_pierwsza; Underscroll notation 
+
+var str = 'aaa';
+var str2 = 'bbb';
+
+var str3, str4, str5, str6;
+
+if(true) {
+
+}
+
+document.write('<b>' + a * 0.22 + '</b>') ;
+
+
+var func1 = function() {
+       return 1 ;
+}
+var bool = 'false';
+var l = 50;
+var i;
+var j;
+var len;
+var hei;
+
+if(typeof bool == 'boolean') {
+      
+}
