581e20431d8b5fe9dbee872f0ebf3f032b33f084
Adam Pawlikowski <pawlikowskiadam@endor.local>
2019-10-28 11:20:46 +0100

    28.10.2019

---
+++ Java.js
@@ -0,0 +1,25 @@
+var text = "Ala ma kota";
+for(let i = 0; i < text.length; i++)
+     document.writeln(text[i]);
+
+var text3 = "Ala bez kota";
+for(let i = 0; i < text3.length; i++)
+     document.writeln(text3[i]);     
+
+var num = 16;
+for(let i = num; i > 0; i--)
+     console.log(i.toString(16));     
+
+var hex = "ce" ;
+alert(parseInt(hex, 16));
+
+var hex = "ce";
+console.log(parseInt(hex, 16));
+
+var float = 109.67;
+console.log(parseInt(float));
+
+var f_text = "399.5677";
+console.log(parseFloat(f_text));
+console.log(parseFloat(f_text) + float);
+console.log(f_text + float) ;
